cbc44fdf324888d1abf50b4c165b29bc06796f4b
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-05-14 18:33:55 -0300

    refactor: components list created

--- src/components/index.js
+++ src/components/index.js
@@ -7,15 +7,23 @@ import renderMoneyBox from "./renderMoneyBox";
 import renderTextAreaObservations from "./renderTextAreaObservations";
 
 export default function  main() {
-  renderRecepcionistInput();
-  renderCalendar();
-  renderLine();
-  renderCounterInputs();
-  renderLine()
-  renderMoneyBox();
-  renderLine()
-  renderTextAreaObservations();
-  renderCopyButton();
+  const components = [
+    renderRecepcionistInput, 
+    renderCalendar, 
+    renderCounterInputs, 
+    renderMoneyBox, 
+    renderTextAreaObservations, 
+    renderCopyButton]
+
+  for(let i=0; i<components.length; i++){
+    
+    components[i]();
+
+    if(i !== 0 && i <5){
+      renderLine()
+    }
+  }
+  
 }
 
 main();
