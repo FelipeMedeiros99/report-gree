29957eb8c2a68e6c446b64d7f91d6fd934081e7d
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-05-13 23:41:28 -0300

    feat: gree-report

---
+++ functions.js
@@ -0,0 +1,147 @@
+function alterValue(element, operation) {
+  const input = element.parentNode.querySelector("input");
+
+  let inputValue = Number(input.value) || 0;
+
+  if (operation === "up") {
+    inputValue++
+  } else if (operation === "down") {
+    if (inputValue > 0) {
+      inputValue--
+    }
+  }
+
+  input.value = String(inputValue);
+}
+
+
+function filterNumbers(string) {
+  string = String(string)
+  return string.replace(/\D/g, "")
+}
+
+
+function numericInputOnChange(component) {
+  let arrayValues = filterNumbers(component.value);
+  component.value = arrayValues;
+}
+
+
+function convertToFloat(stringReceived) {
+
+  return filterNumbers(stringReceived) / 100
+}
+
+function valueSend() {
+  const valueReceived = convertToFloat(document.querySelector("#Caixa").value);
+  const valueRemoved = convertToFloat(document.querySelector("#Retiradas").value)
+  const total = valueReceived - valueRemoved
+
+  document.querySelector("#Caixa_enviado").value = "R$ " + String(total.toFixed(2)).replace(".", ",");
+}
+
+
+function numericFloatInputOnChange(component) {
+  let value = filterNumbers(component.value);
+  value = (Number(value / 100)).toFixed(2)
+
+  component.value = "R$ " + String(value).replace(".", ",")
+
+  valueSend()
+}
+
+
+function saveNewRecepcionist(name) {
+  const names = getRecepcionistsNames();
+  names.push(name)
+  localStorage.setItem("reportSystem", JSON.stringify(names))
+}
+
+
+function getRecepcionistsNames() {
+  let recepcionistList = localStorage.getItem("reportSystem");
+  if (!recepcionistList) {
+    localStorage.setItem("reportSystem", "[]");
+    recepcionistList = "[]";
+  }
+  recepcionistList = JSON.parse(recepcionistList);
+  return recepcionistList.sort();
+}
+
+
+function updateNames() {
+  const recepcionistList = getRecepcionistsNames()
+  const selectedRecepcionist = document.querySelector("#recepcionist-name").value;
+  const recepcionistExists = recepcionistList.indexOf(selectedRecepcionist) > -1
+  if (!recepcionistExists) {
+    saveNewRecepcionist(selectedRecepcionist)
+  }
+}
+
+
+
+function generateReport(listOfDatas) {
+  const report = `
+📋 *Relatório de Plantão - ${formatDate(listOfDatas[1])}*
+👤 *Recepcionista:* ${listOfDatas[0]}
+
+🏨 *Operacional*  
+• Ocupação total: *${listOfDatas[2]}*  
+• Previsão de reservas: *${listOfDatas[3]}*  
+• Check-ins: *${listOfDatas[4]}*  
+• Renovações: *${listOfDatas[5]}*
+
+💰 *Financeiro*  
+• Caixa: *${listOfDatas[6]}*  
+• Retiradas: *${listOfDatas[7]}*  
+• Caixa enviado: *${listOfDatas[8]}*
+
+📝 *Informações*  
+• Devendo: 
+_${listOfDatas[9]}_
+
+• Observações:  
+_${listOfDatas[10]}_
+`
+
+
+
+  navigator.clipboard.writeText(report)
+}
+
+function copyInformations() {
+  updateNames()
+  const valuesReceived = []
+  const inputs = document.querySelectorAll("input")
+  const textareas = document.querySelectorAll("textarea")
+
+  for (let input of inputs) {
+    valuesReceived.push(input.value);
+  }
+  for (let text of textareas) {
+    valuesReceived.push(text.value);
+  }
+
+  generateReport(valuesReceived)
+
+}
+
+
+function clearRecepcionistName() {
+  document.querySelector("#recepcionist-name").value = ""
+}
+
+
+function dateFormat(numbers) {
+  numbers = numbers.slice(0, 8)
+  if (numbers.length > 5) {
+    return numbers.slice(0, 2) + "/" + numbers.slice(2, 4) + "/" + numbers.slice(4)
+  }
+  return numbers
+
+}
+
+function formatDate(defaultData) {
+  const splitedData = defaultData.split("-")
+  return `${splitedData[2]}/${splitedData[1]}/${splitedData[0]}`
+}
