d6b6605aaa76d0425841c7c0f5acb76ed7362783
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-05-15 14:37:53 -0300

    refactor: renderTextArea refactored

--- src/components/renderTextAreaObservations.js
+++ src/components/renderTextAreaObservations.js
@@ -1,11 +1,23 @@
+import { addAtRoot } from "../tools/domFunctions";
+
 export default function renderTextAreaObservations() {
   const labels = ["Devendo", "Observações"]
   for (const label of labels) {
-    document.querySelector(".root").innerHTML += `
-    <div class="container-input">
-      <label for="obs-box">${label}</label>
-      <textarea type="text" class="obs-box"></textarea>
-    </div>
-  `
+    const className = "obs-box"
+    
+    const container = document.createElement("div");
+    container.className = "container-input";
+
+    const legend = document.createElement('label');
+    legend.setAttribute("for", className);
+    legend.textContent = label;
+
+    const textArea = document.createElement("textarea");
+    textArea.setAttribute("type", "text")
+    textArea.className = className;
+
+    container.append(legend, textArea)
+
+    addAtRoot(container)
   }
 }
