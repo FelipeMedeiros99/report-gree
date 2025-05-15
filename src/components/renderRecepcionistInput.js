ee0d5ef1cf07c83bd0d9262b7f1904391b1803e5
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-05-15 13:42:11 -0300

    cleanup: reorganized elements

--- src/components/renderRecepcionistInput.js
+++ src/components/renderRecepcionistInput.js
@@ -1,25 +1,25 @@
-import { addAtRoot } from "../tools/domFunctions";
+import { addAtRoot, clearRecepcionistName } from "../tools/domFunctions";
 import { getRecepcionistsNames } from "../tools/storageFunctions";
 
+
 export default function renderRecepcionistInput() {
-  
   const recepcionistList = getRecepcionistsNames();
-  
   const container = document.createElement("div");
-  
   container.className = "recepcionist-container container-input"
-  
+
   container.innerHTML = `  
       <label for="recepcionist-name">Recepcionista</label>
       <div class="container-input-with-button">
       <input type="text" id="recepcionist-name" list="recepcionists"/>
-      <img src="assets/image.png" class="clearButton" onclick="clearRecepcionistName()"/>
+      <img src="assets/image.png" class="clearButton"/>
       </div>
       
       <datalist id="recepcionists">
-        ${recepcionistList.map((recep)=>(`<option>${recep}</option>`))}
+        ${recepcionistList.map((recep) => (`<option>${recep}</option>`))}
       </datalist>
       `;
-
   addAtRoot(container)
+  const img = container.querySelector("img");
+
+  img.addEventListener("click", () => clearRecepcionistName(container))
 }
