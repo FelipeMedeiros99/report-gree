f54df0a439404aee0556733813b82689ff6edbd2
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-05-15 12:22:12 -0300

    fix: sort erro fixed

--- src/components/renderRecepcionistInput.js
+++ src/components/renderRecepcionistInput.js
@@ -6,6 +6,7 @@ export default function renderRecepcionistInput() {
   const recepcionistList = getRecepcionistsNames();
   
   const container = document.createElement("div");
+  
   container.className = "recepcionist-container container-input"
   
   container.innerHTML = `  
