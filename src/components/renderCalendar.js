b32647fe218e5b9bdfdc5ce41b5039836cc25c69
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-05-15 13:40:34 -0300

    refactor: altered formatarion of component

--- src/components/renderCalendar.js
+++ src/components/renderCalendar.js
@@ -1,13 +1,17 @@
+import { addAtRoot } from "../tools/domFunctions";
+
 export default function renderCalendar(){
   const date = new Date();
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth()+1).padStart(2, "0");
   const year = String(date.getFullYear());
 
-  document.querySelector(".root").innerHTML += `
-    <div class="container-input">
-      <label>Data</label>
-      <input type="date" value="${year}-${month}-${day}"/>
-    </div>
+  const container = document.createElement("div");
+  container.className = "container-input";
+
+  container.innerHTML = `
+    <label>Data</label>
+    <input type="date" value="${year}-${month}-${day}"/>
   `
+  addAtRoot(container)
 }
