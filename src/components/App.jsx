72c76cd8ed79efe0692310f0aadd743c3e6ff944
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-06-05 01:56:41 -0300

    feat: MoneyBox created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import RecepcionistForm from "./RecepcionistForm";
 import Calendar from "./Calendar";
 import CounterInput from "./CounterInput";
+import MoneyBox from "./MoneyBox";
+
+import getDateToday from "../tools/getDateToday";
 
 function App() {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    recepcionist: "",
+    date: getDateToday(),
+    counterInputs: {"Ocupação total": 50, "Previsão reservas": 0, "Check-in": 0, "Renovação": 0},
+    money: { Caixa: "R$ 0,00", Retiradas: "R$ 0,00", "Caixa enviado": "R$ 0,00" },
+  });
 
   console.log(formData)
   return (
     <div className="main">
-      <RecepcionistForm formData={formData} setFormData={setFormData}/>
-      <Calendar formData={formData} setFormData={setFormData}/>
+      <RecepcionistForm formData={formData} setFormData={setFormData} />
+      <Calendar formData={formData} setFormData={setFormData} />
       <hr />
-      <CounterInput formData={formData} setFormData={setFormData}/>
+      <CounterInput formData={formData} setFormData={setFormData} />
+      <hr />
+      <MoneyBox formData={formData} setFormData={setFormData} />
+
     </div>
   );
 }
--- src/components/CounterInput.jsx
+++ src/components/CounterInput.jsx
@@ -1,17 +1,4 @@
-import { useEffect} from "react"
-
 export default function CounterInput({ formData, setFormData }) {
-  useEffect(()=>{
-    if(!formData.counterInputs){
-      const counterInputs = {
-        "Ocupação total": 50,
-        "Previsão reservas": 0,
-        "Check-in": 0,
-        "Renovação": 0
-      }
-      setFormData({...formData, counterInputs: counterInputs})
-    }
-  }, [formData]);
 
   const changeInputValue = (key, value) => {
     const newValue = Number(formData.counterInputs[key]) + value;
@@ -32,7 +19,7 @@ export default function CounterInput({ formData, setFormData }) {
 
       {Object.keys(formData?.counterInputs || []).map((inputLabel) => (
         <div key={inputLabel} className="numeric-input">
-          <label for={inputLabel}>{inputLabel}</label>
+          <label htmlFor={inputLabel}>{inputLabel}</label>
           <div className="buttons-container" id={inputLabel}>
             <button type="button" className="op-button down" onClick={()=>changeInputValue(inputLabel, -1)}>-</button>
             <input type="text" className="input-number" value={formData?.counterInputs[inputLabel]} onChange={(e)=>changeValue(e, inputLabel)}/>
