26fe643c6d544cf8773dcc0d0f5a336e862ebdf7
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-06-04 23:29:05 -0300

    feat: CounterInput created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
+
 import RecepcionistForm from "./RecepcionistForm";
 import Calendar from "./Calendar";
+import CounterInput from "./CounterInput";
 
 function App() {
   const [formData, setFormData] = useState({});
 
+  console.log(formData)
   return (
     <div className="main">
       <RecepcionistForm formData={formData} setFormData={setFormData}/>
       <Calendar formData={formData} setFormData={setFormData}/>
+      <hr />
+      <CounterInput formData={formData} setFormData={setFormData}/>
     </div>
   );
 }
--- src/components/renderCounterInput.js
+++ src/components/renderCounterInput.js
@@ -1,66 +1,66 @@
-// import { addAtRoot, setOnlyNumbersOnInput } from "../tools/domFunctions.js";
+import { addAtRoot, setOnlyNumbersOnInput } from "../tools/domFunctions.js";
 
-// function generateComponent() {
-//   const inputsLabels = {
-//     "Ocupação total": 50,
-//     "Previsão reservas": 0,
-//     "Check-in": 0,
-//     "Renovação": 0
-//   }
+function generateComponent() {
+  const inputsLabels = {
+    "Ocupação total": 50,
+    "Previsão reservas": 0,
+    "Check-in": 0,
+    "Renovação": 0
+  }
 
-//   const container = document.createElement("div");
-//   container.className = "counter-inputs-container container-input";
+  const container = document.createElement("div");
+  container.className = "counter-inputs-container container-input";
 
-//   for (const inputLabel of Object.keys(inputsLabels)) {
-//     const subContainer = document.createElement("div");
-//     subContainer.className = "numeric-input";
+  for (const inputLabel of Object.keys(inputsLabels)) {
+    const subContainer = document.createElement("div");
+    subContainer.className = "numeric-input";
 
-//     subContainer.innerHTML = `
-//     <label for="${inputLabel}">${inputLabel}</label>
-//     <div class="buttons-container" id="${inputLabel}">
-//       <button type="button" class="op-button down">-</button>
-//       <input type="text" class="input-number" value="${inputsLabels[inputLabel]}"/>
-//       <button type="button" class="op-button up">+</button>
-//     </div>
-//     `
+    subContainer.innerHTML = `
+    <label for="${inputLabel}">${inputLabel}</label>
+    <div class="buttons-container" id="${inputLabel}">
+      <button type="button" class="op-button down">-</button>
+      <input type="text" class="input-number" value="${inputsLabels[inputLabel]}"/>
+      <button type="button" class="op-button up">+</button>
+    </div>
+    `
     
-//     container.appendChild(subContainer)
-//   }
+    container.appendChild(subContainer)
+  }
 
-//   addAtRoot(container)
-// }
+  addAtRoot(container)
+}
 
-// function changeInputValue(component, operation) {
+function changeInputValue(component, operation) {
 
-//   const input = component.querySelector("input");
-//   let inputValue = Number(input.value) || 0;
+  const input = component.querySelector("input");
+  let inputValue = Number(input.value) || 0;
 
-//   input.value =
-//     operation === "up"
-//       ? inputValue + 1
-//       : inputValue > 0
-//         ? inputValue - 1
-//         : inputValue
-//     ;
-// }
+  input.value =
+    operation === "up"
+      ? inputValue + 1
+      : inputValue > 0
+        ? inputValue - 1
+        : inputValue
+    ;
+}
 
-// function addEventAtComponents() {
-//   const inputContainers = document.querySelectorAll(".numeric-input");
+function addEventAtComponents() {
+  const inputContainers = document.querySelectorAll(".numeric-input");
 
-//   for (let inputContainer of inputContainers) {
+  for (let inputContainer of inputContainers) {
 
-//     const buttonDown = inputContainer.querySelector(".down")
-//     buttonDown.addEventListener("click", () => changeInputValue(inputContainer, "down"))
+    const buttonDown = inputContainer.querySelector(".down")
+    buttonDown.addEventListener("click", () => changeInputValue(inputContainer, "down"))
 
-//     const input = inputContainer.querySelector(".input-number")
-//     input.addEventListener("input", (e) => setOnlyNumbersOnInput(e.currentTarget))
+    const input = inputContainer.querySelector(".input-number")
+    input.addEventListener("input", (e) => setOnlyNumbersOnInput(e.currentTarget))
 
-//     const buttonUp = inputContainer.querySelector(".up")
-//     buttonUp.addEventListener("click", () => changeInputValue(inputContainer, "up"))
-//   }
-// }
+    const buttonUp = inputContainer.querySelector(".up")
+    buttonUp.addEventListener("click", () => changeInputValue(inputContainer, "up"))
+  }
+}
 
-// export default function renderCounterInputs() {
-//   generateComponent()
-//   addEventAtComponents()
-// }
+export default function renderCounterInputs() {
+  generateComponent()
+  addEventAtComponents()
+}
