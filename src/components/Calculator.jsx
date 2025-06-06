100122e88c1309fb8635386f32e0c11991c017d3
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-06-06 11:44:21 -0300

    feat: forced localStorage save

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,7 +1,10 @@
-export default function Calculator({formData, setFormData}) {
-  
+export default function Calculator({formData, setFormData, calculatorVisibility, setCalculatorVisibility}) {
+
   return (
-    <div className="container-input">
+    <div className={`super-container-calculator ${calculatorVisibility}`}>
+      <div className="container-calculator">
+        <button className="button-close-calculator">X</button>
+      </div>
     </div>
   )
 }
